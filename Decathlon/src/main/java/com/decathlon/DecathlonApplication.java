cf31d70f1411d956f1e669846d668be4d921174a
nani <anvbhaskar@gmail.com>
2018-12-02 13:02:19 +0530

    zipking

--- Decathlon/src/main/java/com/decathlon/DecathlonApplication.java
+++ Decathlon/src/main/java/com/decathlon/DecathlonApplication.java
@@ -1,12 +1,15 @@
 package com.decathlon;
 
-import org.jboss.logging.*;
+import org.jboss.logging.Logger;
 
 import org.jboss.logging.Logger.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -20,4 +23,9 @@ public class DecathlonApplication {
 		logger.log(Level.INFO, "main class");
 		SpringApplication.run(DecathlonApplication.class, args);
 	}
+	
+//	@Bean
+//	public Sampler defaultSampler() {
+//		return new AlwaysSampler();
+//	}
 }
--- Decathlon/src/test/java/com/decathlon/DecathlonApplicationTests.java
+++ Decathlon/src/test/java/com/decathlon/DecathlonApplicationTests.java
@@ -1,16 +1,49 @@
 //package com.decathlon;
 //
-//import org.junit.Test;
+//import javax.ws.rs.core.MediaType;
+//
 //import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
+//import org.mockito.Mockito;
+//import org.skyscreamer.jsonassert.JSONAssert;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
 //import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+//
+//import com.decathlon.controller.ProductController;
+//import com.decathlon.dto.ProductDto;
+//import com.decathlon.service.ProductService;
+//import com.sun.jersey.api.client.RequestBuilder;
+//
 //
 //@RunWith(SpringRunner.class)
-//@SpringBootTest
+//@WebMvcTest(value = ProductController.class, secure = false))
 //public class DecathlonApplicationTests {
 //
-//	@Test
-//	public void contextLoads() {
-//	}
+//	@Autowired
+//	public MockMvc mockMvc;
+//	
+//	@MockBean
+//	public ProductService productService;
+//	
 //
+//	public void getMapping(){
+//		
+//		ProductDto productDto=new ProductDto(1,"laptap","desc","medium","electronics");
+//		
+//		Mockito.when(productService.fetchProductDetailsById(Mockito.anyInt())).thenReturn(productDto);
+//	
+//		RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+//				"/v1/public/products/{productId}").accept(
+//				MediaType.APPLICATION_JSON);
+//
+//		MvcResult mvcResult = mockMvc.perform(requestBuilder).andReturn();
+//		String excepted = " {id:1,productName:Ball,productDecription:Ball descp,productLevel:small,productSport:sports}";
+//
+//		JSONAssert.assertEquals(excepted, mvcResult.getResponse()
+//				.getContentAsString(), false);
+//	}
 //}
