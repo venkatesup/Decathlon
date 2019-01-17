d571617af1c3d5404afb3330d0dd85650a4e10b6
venkatesup <venky.pusarla47@gmail.com>
2019-01-17 21:51:45 +0530

    commited

--- Decathlon/src/main/java/com/decathlon/DecathlonApplication.java
+++ Decathlon/src/main/java/com/decathlon/DecathlonApplication.java
@@ -1,31 +1,29 @@
 package com.decathlon;
 
 import org.jboss.logging.Logger;
-
 import org.jboss.logging.Logger.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import brave.sampler.Sampler;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableEurekaClient
+@EnableFeignClients("com.decathlon")
+// @EnableEurekaClient
 public class DecathlonApplication {
 
-	private static final Logger logger = Logger.getLogger(DecathlonApplication.class);
+	private static final Logger logger = Logger
+			.getLogger(DecathlonApplication.class);
 
 	public static void main(String[] args) {
-		
+
 		logger.log(Level.INFO, "main class");
 		SpringApplication.run(DecathlonApplication.class, args);
 	}
-	
-//	@Bean
-//	public Sampler defaultSampler() {
-//		return new AlwaysSampler();
-//	}
+
+	// @Bean
+	// public Sampler defaultSampler() {
+	// return new AlwaysSampler();
+	// }
 }
--- Decathlon/src/main/java/com/decathlon/controller/ProductController.java
+++ Decathlon/src/main/java/com/decathlon/controller/ProductController.java
@@ -1,12 +1,9 @@
 package com.decathlon.controller;
 
 import java.net.URI;
-
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,63 +15,73 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.decathlon.dto.ProductDto;
+import com.decathlon.dto.StoresDto;
 import com.decathlon.entities.Product;
 import com.decathlon.entities.Store;
 import com.decathlon.repository.ProductRepository;
 import com.decathlon.service.ProductService;
+import com.decathlon.service.StoreServiceFeignProxy;
 
 @RestController
-@RequestMapping(value="/v1/public/products")
+@RequestMapping(value = "/v1/public/products")
 public class ProductController {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
-//	@Autowired
-//	private StoreRepository storeRepository;
-	
+
+	@Autowired
+	private StoreServiceFeignProxy storeServiceProxy;
+
+	// @Autowired
+	// private StoreRepository storeRepository;
+
 	@Autowired
 	private ProductService productService;
 
 	// TODO: Need to fetch storedetails using feign client
-//	@PostMapping("/{storeId}/products")
-//	public ResponseEntity<Product> createProduct(@PathVariable Integer storeId, @RequestBody Product product)
-//			throws Exception {
-//		
-//		Optional<Store> store = storeRepository.findById(storeId);
-//		
-//		if (!store.isPresent()) {
-//			throw new Exception("storeId" + storeId);
-//		}
-//		
-//		product.setAssociatedStores(store.get());
-//		Product savedProdcut = productRepository.save(product);
-//		
-//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{productId}")
-//				.buildAndExpand(savedProdcut.getProductId()).toUri();
-//		
-//		return ResponseEntity.created(uri).build();
-//	}
-	
+	@PostMapping("/{storeId}/products")
+	public ResponseEntity<Product> createProduct(@PathVariable Integer storeId,
+			@RequestBody Product product) throws Exception {
+
+		StoresDto storesDto = storeServiceProxy.findByStoreId(storeId);
+		product.setAssociatedStores(importDo(storesDto));
+		Product savedProdcut = productRepository.save(product);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{productId}")
+				.buildAndExpand(savedProdcut.getProductId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
 	@GetMapping
 	public List<ProductDto> retriveAllProducts() {
-		
 		return productService.fetchAllProducts();
-	
 	}
-	
-	@GetMapping("/{productId}")
-	public ProductDto retriveProduct(@PathVariable("productId") Integer productId) throws Exception {
 
+	@GetMapping("/{productId}")
+	public ProductDto retriveProduct(
+			@PathVariable("productId") Integer productId) throws Exception {
+		StoresDto storesDto = storeServiceProxy.findByStoreId(productId);
+		System.err.println("stores id0" + storesDto.getStoreId() + "name "
+				+ storesDto.getStoreName());
 		return productService.fetchProductDetailsById(productId);
 	}
 
-
 	@DeleteMapping("/{productId}")
-	public void deleteProduct(@PathVariable("productId") Integer productId) throws Exception {
-		
+	public void deleteProduct(@PathVariable("productId") Integer productId)
+			throws Exception {
+
 		productService.deleteProductDetailsById(productId);
-		
+
 	}
-	
+
+	private Store importDo(StoresDto storesDto) {
+		Store store = new Store();
+		store.setStoreCity(storesDto.getStoreCity());
+		store.setStoreName(storesDto.getStoreName());
+		store.setStoreId(storesDto.getStoreId());
+		return store;
+	}
+
 }
